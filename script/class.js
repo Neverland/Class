3f1caa803193646bbbd7d063ba3955904f722abe
Neverland <enix@foxmail.com>
2012-08-04 20:11:05 +0800

    add methods _set_ _get_

--- script/class.js
+++ script/class.js
@@ -3,49 +3,66 @@
  * enix@foxmail.com
  * Date: 2012-7-31
  */
+function Class(init, attributes) {
+	var indicator = arguments.callee, that = this;
 
-function Class(init,attributes){
-	function factory(i,a){
+	function factory(i, a) {
+		a || (a = {});
+		a.toString() === '[object Object]' ? a : {};
 		return [
-			this.init=typeof(i)==='function' ? i:function(){},
-			this.attrs=a.toString()==='[object Object]'?a :{}
+
+			this.init = typeof(i) === 'function' ? i : function () {
+			},
+			this.attrs = a
+
 		];
 	}
-	return function(){
-		var that=this,
-			indicator=arguments.callee,
-			slice=[]å.slice,
-			args=slice.call(arguments,0),
-			ins=new factory(init,attributes),
+
+	return function () {
+		var that = this,
+			indicator = arguments.callee,
+			slice = [].slice,
+			args = slice.call(arguments, 0),
+			ins = new factory(init, attributes),
 			pb,
 			i;
-		if(!(this instanceof indicator)) return ;
-		//这里的逻辑还没有处理好，创建实利必须使用new
-		//if(!(this instanceof indicator)){
-		//	return that=new indicator(args);
-		//}
-		//
-
+		if (!(this instanceof indicator)) {
+			return that = new indicator(args);
+		}
 		ins[0].apply(this, arguments);
-
-		pb=indicator.prototype;
+		pb = indicator.prototype;
 		pb.constructor = indicator;
-
-		for(i in ins[1]){
-			if(ins[1].hasOwnProperty(i) && !pb[i]){
-				pb[i]=function(){
-					var attr=ins[1][i];
-					if(typeof attr ==='function'){
-						return function(){
-							return attr.apply && attr.apply(that,slice.call(arguments,0));
+		for (i in ins[1]) {
+			if (ins[1].hasOwnProperty(i) && !pb[i]) {
+				pb[i] = function () {
+					var attr = ins[1][i];
+					if (typeof attr === 'function') {
+						return function () {
+							return attr.apply && attr.apply(that, slice.call(arguments, 0));
 						}
-					}else{
+					} else {
 						return attr;
 					}
 				}();
 			}
 		}
+		pb._get_ || (
 
+			pb._get_ = function (key) {
+				var attr = pb[key]
+				return attr ? attr : -1;
+			}
+
+		);
+		pb._set_ || (
+
+			pb._set_ = function (key, value) {
+				pb[key] || (pb[key] = value);
+			}
+
+		);
 		return that;
 	}
 }
+
+
